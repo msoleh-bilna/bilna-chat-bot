cd28271ab07bbbf460eb0b06cb03696b12042b06
Mohammad Soleh <mohammad.soleh@bilna.com>
2016-06-08 09:59:00 +0700

    modified respons messages

--- app.js
+++ app.js
@@ -33,17 +33,26 @@ app.get('/webhook', function (req, res) {
 });
 
 app.post('/webhook/', function (req, res) {
-  messaging_events = req.body.entry[0].messaging;
-  for (i = 0; i < messaging_events.length; i++) {
-    event = req.body.entry[0].messaging[i];
-    sender = event.sender.id;
+  var messaging_events = req.body.entry[0].messaging
+  for (var i = 0; i < messaging_events.length; i++) {
+    var event = req.body.entry[0].messaging[i]
+    var sender = event.sender.id
     if (event.message && event.message.text) {
-      text = event.message.text;
-      sendTextMessage(sender, "Text received, echo: "+ text.substring(0, 200));
+      var text = event.message.text
+      if (text === 'Generic') {
+          sendGenericMessage(sender)
+          continue
+      }
+      sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+    }
+    if (event.postback) {
+      var text = JSON.stringify(event.postback)
+      sendTextMessage(sender, "Postback received: "+text.substring(0, 200), token)
+      continue
     }
   }
-  res.sendStatus(200);
-});
+  res.sendStatus(200)
+})
 
 var token = "EAAXLNrHrrHQBAE20bSJeEJNkZB0GrcEVtZCXYkzMJTCfAnlHUAL9RJZBZB75FIZAjZAhoJHRb8gO7r3CtCfmK3OE6xhMReBbzWTzSmQNzszmPOVnEz4OmGrfBwc5XUkbIouvfRX2SwTLZBuXayryKdLZCYKzDVXwkQchVblrYnyP19ZAujkLhZBwqF";
 
@@ -66,6 +75,55 @@ function sendTextMessage(sender, text) {
       console.log('Error: ', response.body.error);
     }
   });
+}
+
+function sendGenericMessage(sender) {
+  var messageData = {
+      "attachment": {
+          "type": "template",
+          "payload": {
+              "template_type": "generic",
+              "elements": [{
+                  "title": "First card",
+                  "subtitle": "Element #1 of an hscroll",
+                  "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
+                  "buttons": [{
+                      "type": "web_url",
+                      "url": "https://www.messenger.com",
+                      "title": "web url"
+                  }, {
+                      "type": "postback",
+                      "title": "Postback",
+                      "payload": "Payload for first element in a generic bubble",
+                  }],
+              }, {
+                  "title": "Second card",
+                  "subtitle": "Element #2 of an hscroll",
+                  "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
+                  "buttons": [{
+                      "type": "postback",
+                      "title": "Postback",
+                      "payload": "Payload for second element in a generic bubble",
+                  }],
+              }]
+          }
+      }
+  }
+  request({
+      url: 'https://graph.facebook.com/v2.6/me/messages',
+      qs: {access_token:token},
+      method: 'POST',
+      json: {
+          recipient: {id:sender},
+          message: messageData,
+      }
+  }, function(error, response, body) {
+      if (error) {
+          console.log('Error sending messages: ', error)
+      } else if (response.body.error) {
+          console.log('Error: ', response.body.error)
+      }
+  })
 }
 
 // catch 404 and forward to error handler

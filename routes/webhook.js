1951ea9b4aa256894f1d6229b733fa41caabbc80
Mohammad Soleh <mohammad.soleh@bilna.com>
2016-06-09 14:58:38 +0700

    modified webhook

--- routes/webhook.js
+++ routes/webhook.js
@@ -16,34 +16,32 @@ router.get('/', function(req, res) {
 router.post('/', function (req, res) {
   var events = req.body.entry[0].messaging;
   for (i = 0; i < events.length; i++) {
-    if (events[i].message) {
-        var event = events[i];
-        if (event.message && event.message.text) {
-            sendTextMessage(event.sender.id, event.message.text);
-        }
+    var event = events[i];
+    if (event.message && event.message.text) {
+      sendTextMessage(event.sender.id, event.message.text);
     }
-    res.send(200);
   }
   res.send(200);
 })
 
 function sendTextMessage(sender, text) {
+  console.log(text)
   if (text.toLowerCase() == "ping") {
     text = {text: "Echo: " + text }
     request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
-        method: 'POST',
-        json: {
-            recipient: {id: recipientId},
-            message: message,
-        }
+      url: 'https://graph.facebook.com/v2.6/me/messages',
+      qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+      method: 'POST',
+      json: {
+        recipient: {id: recipientId},
+        message: message,
+      }
     }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending message: ', error);
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error);
-        }
+      if (error) {
+        console.log('Error sending message: ', error);
+      } else if (response.body.error) {
+        console.log('Error: ', response.body.error);
+      }
     });
   }
 }

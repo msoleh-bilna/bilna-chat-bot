53eb5414af46901247cff7e6bebbfa85dcf6ba94
Mohammad Soleh <mohammad.soleh@bilna.com>
2016-06-10 16:26:31 +0700

    modified webhook

--- routes/webhook.js
+++ routes/webhook.js
@@ -31,9 +31,9 @@ router.post('/', jsonParser, function (req, res) {
 function sendTextMessage(sender, text) {
   console.log('outside if: '+text)
   if (text.toLowerCase() === "ping") {
-    var recipient = {id: sender}
-    var message = {text: "hello, "+text}
-    var data = {recipient : recipient, message: message}
+    var recipient = '{id: '+sender+'}'
+    var message = '{text: "hello, "'+text+'}'
+    var data = '{recipient : '+recipient+', message: '+message+'}'
     var cmd = 'curl -X POST -H "Content-Type: application/json" -d \''+data+'\' "https://graph.facebook.com/v2.6/me/messages?access_token='+process.env.PAGE_ACCESS_TOKEN+'"';
     console.log('send command: '+cmd);
     console.log('inside if after cmd: '+text)

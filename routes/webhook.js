76deebd28dba8fa884de9da57f5ae6bd9123b1d1
Mohammad Soleh <mohammad.soleh@bilna.com>
2016-06-10 16:38:21 +0700

    modified webhook

--- routes/webhook.js
+++ routes/webhook.js
@@ -33,7 +33,7 @@ function sendTextMessage(sender, text) {
   if (text.toLowerCase() === "ping") {
     var recipient = '{"id": "'+sender+'"}'
     var message = '{"text": "hello, "'+text+'"}'
-    var data = '{"recipient": "'+recipient+'", "message": "'+message+'"}'
+    var data = '{"recipient": '+recipient+', "message": '+message+'}'
     var cmd = 'curl -X POST -H "Content-Type: application/json" -d \''+data+'\' "https://graph.facebook.com/v2.6/me/messages?access_token='+process.env.PAGE_ACCESS_TOKEN+'"';
     console.log('send command: '+cmd);
     console.log('inside if after cmd: '+text)

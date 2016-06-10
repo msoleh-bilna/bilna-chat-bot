895a3157c8ff873b9610be7510daccc9e9d04576
Mohammad Soleh <mohammad.soleh@bilna.com>
2016-06-10 17:24:55 +0700

    modified webhook

--- routes/webhook.js
+++ routes/webhook.js
@@ -14,6 +14,7 @@ router.get('/', function(req, res) {
   }
 });
 
+// curl -i -X POST -H 'Content-Type: application/json' -d '{"object":"page","entry":[{"id":43674671559,"time":1460620433256,"messaging":[{"sender":{"id":1456789},"recipient":{"id":987654321},"timestamp":1460620433123,"message":{"mid":"mid.1460620432888:f8e3412003d2d1cd93","seq":12604,"text":"Testing Chat Bot .."}}]}]}' https://bilna-chat-bot.herokuapp.com/webhook
 // var token = "EAAXLNrHrrHQBAMWiEdoVzZCUu5X5rkzjavEpnZBXbA4yZAsPZBa4UzXsmlU9sxPTo5Se8t61yuvZAnZAJHP4jH6IQtVKGME5S8pGkBzwPG1k3F2Y9m7ioWrfhZAUN8lffjAUGkLZBwQaoB1Mj1HfmeSNHY5XLpZBcjbHoZAcJSBJwEK5QHZBdLE7zZBC";
 
 router.post('/', jsonParser, function (req, res) {
@@ -34,12 +35,15 @@ function sendTextMessage(sender, text) {
     var recipient = '{"id": "'+sender+'"}'
     var message = '{"text": "hello, '+text+'"}'
     var data = '{"recipient": '+recipient+', "message": '+message+'}'
+    var subscribe = 'curl -X POST "https://graph.facebook.com/v2.6/me/subscribed_apps?access_token='+process.env.PAGE_ACCESS_TOKEN+'"'
     var cmd = 'curl -X POST -H "Content-Type: application/json" -d \''+data+'\' "https://graph.facebook.com/v2.6/me/messages?access_token='+process.env.PAGE_ACCESS_TOKEN+'"';
+    exec(subscribe, function(error, stdout, stderr) {
+      exec(cmd, function(error, stdout, stderr) {
+        // console.log('Masuk exec, stdout: '+stdout)
+      });
+    });
     // console.log('send command: '+cmd);
     // console.log('inside if after cmd: '+text)
-    exec(cmd, function(error, stdout, stderr) {
-      // console.log('Masuk exec, stdout: '+stdout)
-    });
   // }
 }
 

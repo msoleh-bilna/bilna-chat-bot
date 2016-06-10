f080bb902b491b299b92430f36ca307d02564045
Mohammad Soleh <mohammad.soleh@bilna.com>
2016-06-10 15:21:22 +0700

    modified webhook

--- routes/webhook.js
+++ routes/webhook.js
@@ -6,13 +6,13 @@ var jsonParser = bodyParser.json();
 var exec = require('child_process').exec;
 
 /* GET home page. */
-// router.get('/', function(req, res) {
-//   if (req.query['hub.verify_token'] === process.env.VERIFY_TOKEN) {
-//     res.send(req.query['hub.challenge']);
-//   } else {
-//     res.send('Error, wrong validation token');
-//   }
-// });
+router.get('/', function(req, res) {
+  if (req.query['hub.verify_token'] === process.env.VERIFY_TOKEN) {
+    res.send(req.query['hub.challenge']);
+  } else {
+    res.send('Error, wrong validation token');
+  }
+});
 
 // var token = "EAAXLNrHrrHQBAMWiEdoVzZCUu5X5rkzjavEpnZBXbA4yZAsPZBa4UzXsmlU9sxPTo5Se8t61yuvZAnZAJHP4jH6IQtVKGME5S8pGkBzwPG1k3F2Y9m7ioWrfhZAUN8lffjAUGkLZBwQaoB1Mj1HfmeSNHY5XLpZBcjbHoZAcJSBJwEK5QHZBdLE7zZBC";
 
@@ -31,7 +31,10 @@ router.post('/', jsonParser, function (req, res) {
 function sendTextMessage(sender, text) {
   console.log('outside if: '+text)
   if (text.toLowerCase() === "ping") {
-    var cmd = 'curl -X POST -H "Content-Type: application/json" -d \'{"recipient":{"id":"'+recipientId+'"}, "message":{"text":"hello, '+text+'!"}}\' "https://graph.facebook.com/v2.6/me/messages?access_token='+process.env.PAGE_ACCESS_TOKEN+'"';
+    var recipient = {id: sender}
+    var message = {text: "hello, "+text}
+    var data = {recipient : recipient, message: message}
+    var cmd = 'curl -X POST -H "Content-Type: application/json" -d \''+data+'\' "https://graph.facebook.com/v2.6/me/messages?access_token='+process.env.PAGE_ACCESS_TOKEN+'"';
     console.log('send command: '+cmd);
     console.log('inside if after cmd: '+text)
     exec(cmd, function(error, stdout, stderr) {
